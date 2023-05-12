40ca1c26fe84106598d65fb8468e76c531337a1e
Philosfin <54917743+Philosfin@users.noreply.github.com>
2023-05-12 15:09:29 +0200

    add config.py and db connection

---
+++ config.py
@@ -0,0 +1,4 @@
+uri = "mongodb+srv://username:password@cluster0.j9iuwdr.mongodb.net/?retryWrites=true&w=majority"
+api_key = "D0zG9dlhQJ6Z2JxnquQxiNj594Z3jIZCaouFkgVjwOvaJfYEYvfDY9sm6CZnfZh2"
+username = "syaltdev"
+password = "PdngE8HvLgDBy8jN"
